b8a6b82fe76101eb0aa32d20151d5ca06e5ca0da
Alireza Latifi <l.alirezalatifi@gmail.com>
2022-06-12 19:01:46 +0430

    calender and progress bar and tab swiches completed

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
 "use strict";
-console.log('connected');
+console.log("connected");
+
+// //  Menu Tab Switches
+const menuTabList = document.querySelectorAll(".nav__link");
+const mainContentList = document.querySelectorAll(".main__content");
+
+menuTabList.forEach((item, index) => {
+    item.addEventListener("click", function () {
+        menuTabList.forEach((item) => {
+            item.classList.remove("nav__link--active");
+        });
+
+        mainContentList.forEach((item) => {
+            item.classList.remove("main__content--active");
+        });
+
+        item.classList.add("nav__link--active");
+        item.blur();
+
+        mainContentList[index].classList.add("main__content--active");
+    });
+});
+
+// Calender
+const today = new Date().setHours(0, 0, 0, 0);
+const defaultOptions = {
+    defaultViewDate: today,
+    nextArrow: '<i class="fa-solid fa-angle-right"></i>',
+    prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+};
+const elem = document.getElementById("inline");
+const datepicker = new Datepicker(elem, defaultOptions);
